95ae08dc42353b8afc17a0947750d4962322a79e
Kevin Angulo <angulo.kevin10@gmail.com>
2025-02-13 15:09:40 -0700

    Created JSX Files of each "page"

---
+++ src/dashlinkDashboard/dashlinkDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function DashlinkDashboard() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>DashlinkDashboard displayed here</div>
+    </main>
+  );
+}
---
+++ src/publicLink/publicLink.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function PublicLink() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>PublicLink displayed here</div>
+    </main>
+  );
+}
