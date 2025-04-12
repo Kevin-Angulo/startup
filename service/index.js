f12f78078c11940daf5c60257ba366c17090a882
Kevin Angulo <angulo.kevin10@gmail.com>
2025-04-12 15:55:59 -0600

    Use Simon's Auth template for backend endpoints

--- service/index.js
+++ service/index.js
@@ -4,91 +4,103 @@ const uuid = require("uuid");
 const bcrypt = require("bcryptjs");
 
 const app = express();
-app.use(express.json()); // Parse JSON in requests
-app.use(cookieParser()); // Read/write cookies
+app.use(express.json()); // JSON body parsing using built-in middleware
+app.use(cookieParser()); // Use the cookie parser middleware for tracking authentication tokens
 
 const users = []; // Temporary in-memory "database"
 
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.static("public")); // Serve up the front-end static content hosting
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
+apiRouter.post("/auth/create", async (req, res) => {
+  if (await findUser("email", req.body.email)) {
+    res.status(409).send({ msg: "Existing user" });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post("/auth/login", async (req, res) => {
+  const user = await findUser("email", req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: "Unauthorized" });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete("/auth/logout", async (req, res) => {
+  const user = await findUser("token", req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser("token", req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: "Unauthorized" });
+  }
+};
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile("index.html", { root: "public" });
+});
+
 async function createUser(email, password) {
   const passwordHash = await bcrypt.hash(password, 10);
+
   const user = {
     email: email,
     password: passwordHash,
+    token: uuid.v4(),
   };
   users.push(user);
+
   return user;
 }
 
-async function getUser(field, value) {
-  if (value) {
-    return users.find((u) => u[field] === value);
-  }
-  return null;
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
 }
 
-function setAuthCookie(res, user) {
-  user.token = uuid.v4(); // generate a secure token
-
-  res.cookie("token", user.token, {
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
     secure: true,
     httpOnly: true,
     sameSite: "strict",
   });
 }
-
-function clearAuthCookie(res, user) {
-  delete user.token;
-  res.clearCookie("token");
-}
-
-// Register
-app.post("/api/auth", async (req, res) => {
-  if (await getUser("email", req.body.email)) {
-    res.status(409).send({ msg: "Existing user" });
-  } else {
-    const user = await createUser(req.body.email, req.body.password);
-    setAuthCookie(res, user);
-    res.send({ email: user.email });
-  }
-});
-
-// Login
-app.put("/api/auth", async (req, res) => {
-  const user = await getUser("email", req.body.email);
-  if (user && (await bcrypt.compare(req.body.password, user.password))) {
-    setAuthCookie(res, user);
-    res.send({ email: user.email });
-  } else {
-    res.status(401).send({ msg: "Unauthorized" });
-  }
-});
-
-// Logout
-app.delete("/api/auth", async (req, res) => {
-  const token = req.cookies["token"];
-  const user = await getUser("token", token);
-  if (user) {
-    clearAuthCookie(res, user);
-  }
-  res.send({});
-});
-
-// Get current user
-app.get("/api/user/me", async (req, res) => {
-  const token = req.cookies["token"];
-  const user = await getUser("token", token);
-  if (user) {
-    res.send({ email: user.email });
-  } else {
-    res.status(401).send({ msg: "Unauthorized" });
-  }
-});
-
-
-
-
-// Port setup (required)
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
